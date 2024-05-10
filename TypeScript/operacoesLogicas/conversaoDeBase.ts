a5147cb0a21abd5f7d312f4d1619ac32cdf04d8c
Davi Soares <davisfs2110@gmail.com>
2024-05-09 21:16:06 -0300

    feat: Adiciona o menu principal da conversão de bases

--- TypeScript/operacoesLogicas/conversaoDeBase.ts
+++ TypeScript/operacoesLogicas/conversaoDeBase.ts
@@ -1,3 +1,17 @@
 export function conversaoDeBase() {
-  // Lógica aqui
+   let opcao: number;
+
+   console.log("______________________________");
+   console.log("|    Conversor de bases      |");
+   console.log("|----------------------------|");
+   console.log("| 1 - B10 x B2               |");
+   console.log("| 2 - B2 x B10               |");
+   console.log("| 3 - B2 x B8                |");
+   console.log("| 4 - B8 x B2                |");
+   console.log("| 5 - B2 x B16               |");
+   console.log("| 6 - B16 x B2               |");
+   console.log("| 0 - Voltar                 |");
+   console.log("|____________________________|");
+   console.log("| Selecione uma conversao:");
+   opcao = Number(prompt());
 }
