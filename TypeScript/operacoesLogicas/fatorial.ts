5ab3ae0c54c98f456b642bbd10369f1218d06ea5
joaopaulista <jvpaulistaa@gmail.com>
2024-05-20 17:04:24 -0300

    lógica fatorial
    adicionado fatorial

--- TypeScript/operacoesLogicas/fatorial.ts
+++ TypeScript/operacoesLogicas/fatorial.ts
@@ -1,3 +1,15 @@
-export function fatorial() {
-  // Lógica aqui
+import * as readlineSync from 'readline-sync';
+
+let numeroString = readlineSync.question("Digite um número para calcular o fatorial: ");
+let numero = parseInt(numeroString);
+
+if (isNaN(numero) || numero < 0) {
+    console.log("Por favor, insira um número inteiro não negativo.");
+} else {
+    let fatorial = 1;
+    for (let i = 1; i <= numero; i++) {
+        fatorial *= i;
+    }
+    console.log(`O fatorial de ${numero} é ${fatorial}.`);
 }
+
