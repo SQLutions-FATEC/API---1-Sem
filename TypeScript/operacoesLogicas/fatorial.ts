8604a4bf579f97c25c31dca4342d49eeb9685d55
joaopaulista <jvpaulistaa@gmail.com>
2024-05-20 22:13:38 -0300

    fix: correção do método de input e síntaxe de função

--- TypeScript/operacoesLogicas/fatorial.ts
+++ TypeScript/operacoesLogicas/fatorial.ts
@@ -1,15 +1,15 @@
-import * as readlineSync from 'readline-sync';
+export function fatorial() {
+    const input=require("prompt-sync")();
 
-let numeroString = readlineSync.question("Digite um número para calcular o fatorial: ");
-let numero = parseInt(numeroString);
-
-if (isNaN(numero) || numero < 0) {
-    console.log("Por favor, insira um número inteiro não negativo.");
-} else {
-    let fatorial = 1;
-    for (let i = 1; i <= numero; i++) {
-        fatorial *= i;
+    let numero = parseInt(input("Digite um número para calcular o fatorial: "));
+    
+    if (isNaN(numero) || numero < 0) {
+        console.log("Por favor, insira um número inteiro não negativo.");
+    } else {
+        let fatorial = 1;
+        for (let i = 1; i <= numero; i++) {
+            fatorial *= i;
+        }
+        console.log(`O fatorial de ${numero} é ${fatorial}.`);
     }
-    console.log(`O fatorial de ${numero} é ${fatorial}.`);
 }
-
