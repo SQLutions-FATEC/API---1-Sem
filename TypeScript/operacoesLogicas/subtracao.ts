9d0a6d8ab0b82e20ec650b3bd5f898eec6cbd970
BryanARMatheus <bryanassis244@gmail.com>
2024-05-22 01:53:36 -0300

    feat: Adiciona a função de subtração

--- TypeScript/operacoesLogicas/subtracao.ts
+++ TypeScript/operacoesLogicas/subtracao.ts
@@ -1,3 +1,24 @@
 export function subtracao() {
-  // Lógica aqui
+  let input = require("prompt-sync")(); 
+  let resultado: number, numero2: number , numero: number;
+
+    console.log('_________________________________');
+    console.log('|           Subtracao           |');
+    console.log('|-------------------------------|');
+    console.log('| Digite o primeiro numero:     |');
+    numero = Number(input('| '));
+    console.log('| Digite o segundo numero:      |');
+    numero2 = Number(input('| '));
+    console.log('|-------------------------------|');
+
+    resultado = numero - numero2;
+    if (isNaN(resultado)){
+      console.log('| Resultado invalido            |')
+      console.log('| Razao: valores nao numericos  |')
+    }
+    else{
+      console.log('| Resultado:                    |');
+      console.log('| ',numero, '-',numero2, '=', resultado);
+    }
+    console.log('|_______________________________|');
 }
