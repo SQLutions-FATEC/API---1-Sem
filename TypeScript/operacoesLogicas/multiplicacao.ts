b99b1eeaf7cf4277f01f759e630d5654032cbd1d
Enzo L. <fenzolemos@gmail.com>
2024-05-22 18:16:02 -0300

    feat: Adiciona a função de multiplicação

--- TypeScript/operacoesLogicas/multiplicacao.ts
+++ TypeScript/operacoesLogicas/multiplicacao.ts
@@ -1,3 +1,21 @@
 export function multiplicacao() {
-  // Lógica aqui
+  let inputNum = require("prompt-sync")();
+  let res: number;
+  let numero1: number;
+  let numero2: number;
+  
+    console.log(" ________________________________ ");
+    console.log("|          Multiplicação         |");
+    console.log("|--------------------------------|");
+    console.log("| Digite o primeiro número:      |");
+    numero1 = Number(inputNum("| "));
+    console.log("| Digite o segundo número:       |");
+    numero2 = Number(inputNum("| "));
+    console.log("|--------------------------------|");
+  
+    res = numero1 * numero2;
+    console.log("| Resultado:                     |");
+    console.log(`| ${numero1} * ${numero2} = ${res}`);
+    console.log("|________________________________|");
+
 }
