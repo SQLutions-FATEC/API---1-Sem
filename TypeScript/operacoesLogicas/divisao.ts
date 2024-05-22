7a0961f81b3ee432ff7cd21eb8152f09e236af67
Enzo L. <fenzolemos@gmail.com>
2024-05-22 18:13:37 -0300

    feat: Adiciona a função de divisão

--- TypeScript/operacoesLogicas/divisao.ts
+++ TypeScript/operacoesLogicas/divisao.ts
@@ -1,3 +1,22 @@
 export function divisao() {
-  // Lógica aqui
+  let inputNum = require("prompt-sync")();
+  let res: number;
+  let numero1: number;
+  let numero2: number;
+
+  console.log(" ____________________________________ ");
+  console.log("|               Divisão              |");
+  console.log("|------------------------------------|");
+  console.log("| Digite o primeiro número:          |");
+  numero1 = Number(inputNum("| "));
+  console.log("| Digite o segundo número:           |");
+  numero2 = Number(inputNum("| "));
+  console.log("|------------------------------------|");
+
+
+  res = numero1 / numero2;
+  console.log("| Resultado:                         |");
+  console.log(`| ${numero1} / ${numero2} = ${res}`);
+  console.log("|____________________________________|");
+
 }
