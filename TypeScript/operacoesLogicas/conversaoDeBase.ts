882decc44e5dc60337078c51c582084a3e275a30
augustopiatto <ampiatto@hotmail.com>
2024-05-24 23:49:08 -0300

    fix(conversao-base): remove metodos nativos do js para calculo

--- TypeScript/operacoesLogicas/conversaoDeBase.ts
+++ TypeScript/operacoesLogicas/conversaoDeBase.ts
@@ -1,7 +1,43 @@
 const input = require("prompt-sync")();
 
+function converteBaseNumerica(
+  valor: string,
+  baseInicial: number,
+  baseFinal: number
+) {
+  const faixa =
+    "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ+/".split(
+      ""
+    );
+  const faixaInicial = faixa.slice(0, baseInicial);
+  const faixaFinal = faixa.slice(0, baseFinal);
+
+  var valorDecimal = valor
+    .split("")
+    .reverse()
+    .reduce(function (acumulador: number, digito: string, index: number) {
+      if (faixaInicial.indexOf(digito) === -1)
+        console.log(
+          "Dígito inválido `" + digito + "` para base " + baseInicial + "."
+        );
+      return (acumulador +=
+        faixaInicial.indexOf(digito) * Math.pow(baseInicial, index));
+    }, 0);
+
+  let novoValor: string = "";
+  while (valorDecimal > 0) {
+    novoValor = faixaFinal[valorDecimal % baseFinal] + novoValor;
+    valorDecimal = (valorDecimal - (valorDecimal % baseFinal)) / baseFinal;
+  }
+  return novoValor || "0";
+}
+
 export function conversaoDeBase() {
   let opcaoBases: number;
+  let numeroBinario: string;
+  let numeroBaseOito: string;
+  let numeroDecimal: string;
+  let numeroBaseDezesseis: string;
 
   console.log(" ________________________________ ");
   console.log("|      Conversor de bases        |");
@@ -21,42 +57,69 @@ export function conversaoDeBase() {
   switch (opcaoBases) {
     case 1:
       //B10 x B2
+      console.clear();
+      console.log(`| Opção escolhida: B10 x B2      |`);
       console.log("| Digite um número em base 10:   |");
-      const decimalNumber = Number(input("| "));
-      console.log("| B10 x B2: ", decimalNumber.toString(2));
+      numeroDecimal = input("| ");
+
+      numeroBinario = converteBaseNumerica(numeroDecimal, 10, 2);
+
+      console.log("| O número em binário: ", numeroBinario);
       break;
     case 2:
       //B2 x B10
-      console.log("| Digite um número em base 2:   |");
-      const binaryNumber = input("| ");
-      console.log("| B2 x B10: ", parseInt(binaryNumber, 2));
+      console.clear();
+      console.log(`| Opção escolhida: B2 x B10      |`);
+      console.log("| Digite um número em base 2:    |");
+      numeroBinario = input("| ");
+
+      numeroDecimal = converteBaseNumerica(numeroBinario, 2, 10);
+
+      console.log("| O número em decimal: ", numeroDecimal);
       break;
     case 3:
       //B2 x B8
-      console.log("| Digite um número em base 2:   |");
-      const binaryNumberForOctal = input("| ");
-      console.log("| B2 x B8: ", parseInt(binaryNumberForOctal, 2).toString(8));
+      console.clear();
+      console.log(`| Opção escolhida: B2 x B8       |`);
+      console.log("| Digite um número em base 2:    |");
+      numeroBinario = input("| ");
+
+      numeroBaseOito = converteBaseNumerica(numeroBinario, 2, 8);
+
+      console.log("| O número em octal: ", numeroBaseOito);
       break;
     case 4:
       //B8 x B2
-      console.log("| Digite um número em base 8:   |");
-      const octalNumber = input("| ");
-      console.log("| B8 x B2");
+      console.clear();
+      console.log(`| Opção escolhida: B8 x B2       |`);
+      console.log("| Digite um número em base 8:    |");
+      numeroBaseOito = input("| ");
+
+      numeroBinario = converteBaseNumerica(numeroBaseOito, 8, 2);
+
+      console.log("| O número em binário", numeroBinario);
       break;
     case 5:
       //B2 x B16
-      console.log("| Digite um número em base 2:   |");
-      const binaryNumberForHex = input("| ");
-      console.log(
-        "| B2 x B16: ",
-        parseInt(binaryNumberForHex, 2).toString(16).toUpperCase()
-      );
+      console.clear();
+      console.log(`| Opção escolhida: B2 x B16      |`);
+      console.log("| Digite um número em base 2:    |");
+      numeroBinario = input("| ");
+
+      numeroBaseDezesseis = converteBaseNumerica(numeroBinario, 2, 16);
+
+      console.log("| O número em hexadecimal: ", numeroBaseDezesseis);
       break;
     case 6:
       //B16 x B2
+      console.clear();
+      console.log(`| Opção escolhida: B16 x B2      |`);
       console.log("| Digite um número em base 16:   |");
-      const hexNumber = input("| ");
-      console.log("| B16 x B2: ", parseInt(hexNumber, 16).toString(2));
+      numeroBaseDezesseis = input("| ");
+
+      numeroBinario = converteBaseNumerica(numeroBaseDezesseis, 16, 2);
+
+      console.log("| O número em binário: ", numeroBinario);
       break;
     case 0:
       console.log("| Voltando...                    |");
